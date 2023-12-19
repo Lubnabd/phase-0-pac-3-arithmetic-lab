0c7c8f04a89137df135f5ddad31ae649a9105cdb
Lubnabd <lubna.abdelkhaleq@gmail.com>
2023-12-19 20:38:50 +0300

    completed assigmnet

---
+++ index.js
@@ -0,0 +1,16 @@
+const add = (n, m ) => n+m ; 
+ 
+const subtract = (n, m ) => n-m ; 
+
+const multiply = (n, m ) => n*m ; 
+
+const divide = (n, m ) => n/m ; 
+
+const increment = (n) => ++n ; 
+
+const decrement = (n) => --n ; 
+
+const makeInt=(n)=>parseInt(n, 10);
+
+const preserveDecimal= (n)=>parseFloat(n);
+
